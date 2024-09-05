eda71797a8f624a1f53384f9523bf8522e07613b
Papripa <deborahpyx010404@gmail.com>
2024-09-06 01:15:48 +0900

    L1try

--- line.cpp
+++ line.cpp
@@ -1,7 +1,13 @@
 //Bresenham’s Line Drawing Algorithm
 #include "line.h"
-
-void line(int x0, int y0, int x1, int y1)
+#include "tgaimage.h"
+//tga view : https://schmittl.github.io/tgajs/
+void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color)
 {
-
+	for (float i = 0; i < 1.; i += 0.1)
+	{
+		int x = x0 + (x1 - x0) * i;
+		int y = y0 + (y1 - y0) * i;
+		image.set(x, y, color);
+	}
 }
--- line.h
+++ line.h
@@ -1,6 +1,7 @@
+#include "tgaimage.h"
 #ifndef _LINE_H_
 #define _LINE_H_
-
+void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color);
 //class line {
 //
 //	float x1, x2, y1, y2;
